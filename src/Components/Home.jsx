41953fb301bf028d989e6127275b9e128b659c40
Lucas <lucaskeller98@gmail.com>
2020-11-21 16:45:46 +0100

    fixing ntfs bug part 1/2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BTree from '../js/BTree';
+import BTree from './../js/BTree1';
 import { everyKeyCanBeDeleted, isNumber, sleep } from '../js/helpers';
 import { Graphviz } from 'graphviz-react';
 import {
